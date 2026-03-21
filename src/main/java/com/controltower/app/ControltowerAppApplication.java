f6b57883e158e89138fe175ed928b88c163cbfca
Kevin Luciano <luciano19940@hotmail.com>
2026-03-20 23:01:26 -0600

    Initial commit

---
+++ src/main/java/com/controltower/app/ControltowerAppApplication.java
@@ -0,0 +1,13 @@
+package com.controltower.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControltowerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControltowerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controltower/app/ControltowerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controltower.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControltowerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
